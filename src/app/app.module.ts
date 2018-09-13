1e14cb879f5d254dcfd450c9f79285ace4b54977
Patrick Wachira <patrick.wachira@tracom.co.ke>
2018-09-13 12:23:52 +0300

    Fixed Linux Build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { StewardClientService } from 'steward-client';
+import { StewardClientModule, StewardClientService } from 'steward-client';
 
 @NgModule({
   declarations: [
