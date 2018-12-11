c2cf75c9e8d0be55df46463c544a9bba01fba039
Owori Juma <owori.juma@tracom.co.ke>
2018-12-11 09:58:13 +0300

    datatable bug fix

--- dist/steward-client/public_api.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './lib/steward-client.service';
-export * from './lib/steward-client.component';
-export * from './lib/mlk-datatable/mlk-datatable.component';
-export * from './lib/steward-client.module';
-export * from './lib/entities/wrappers/mlk-dynamic-control';
-export * from './lib/entities/wrappers/page';
-export * from './lib/entities/wrappers/response-wrapper';
