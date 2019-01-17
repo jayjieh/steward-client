4fc1e79de19222b38d9d4e3df02ee17fa13c28cb
Owori Juma <owori.juma@tracom.co.ke>
2019-01-17 14:25:44 +0300

    fixed provider on module

--- projects/steward-client/src/lib/steward-client.module.ts
+++ projects/steward-client/src/lib/steward-client.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
-import { StewardClientComponent } from './steward-client.component';
-import { MlkDatatableComponent } from './mlk-datatable/mlk-datatable.component';
-import { CommonModule } from '@angular/common';
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { StewardConfig } from './steward-client.service';
-import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import {NgModule} from '@angular/core';
+import {StewardClientComponent} from './steward-client.component';
+import {MlkDatatableComponent} from './mlk-datatable/mlk-datatable.component';
+import {CommonModule, DatePipe} from '@angular/common';
+import {NgxDatatableModule} from '@swimlane/ngx-datatable';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {StewardConfig} from './steward-client.service';
+import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
 
 @NgModule({
   imports: [
@@ -18,13 +18,14 @@ import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
     HttpClientModule
   ],
   declarations: [StewardClientComponent, MlkDatatableComponent],
-  exports: [StewardClientComponent, MlkDatatableComponent]
+  exports: [StewardClientComponent, MlkDatatableComponent],
+  providers: [DatePipe]
 })
 export class StewardClientModule {
   static forRoot(config: StewardConfig) {
     return {
       ngModule: StewardClientModule,
-      providers: [ {provide: StewardConfig, useValue: config} ]
-    }
+      providers: [{provide: StewardConfig, useValue: config}]
+    };
   }
- }
+}
